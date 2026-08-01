5e4be594f6c8e9b642fcd7325c4f689139bb19cc
jaswantsurya-coder <srijaswantsuryacherri@gmail.com>
2026-08-01 19:03:51 +0530

    fix: vite build crash on Vercel by suppressing warnings

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return
+        if (warning.code === 'SOURCEMAP_ERROR') return
+        warn(warning)
+      }
+    }
+  }
 })
